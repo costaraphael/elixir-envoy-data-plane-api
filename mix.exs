bd098fad1da9b9c9613377f233df0cbd09f4c7fd
costaraphael <raphael@telnyx.com>
2024-01-17 13:55:32 +0000

    Published version 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,12 +5,14 @@ defmodule EnvoyDataPlaneApi.MixProject do
     [
       app: :envoy_data_plane_api,
       version: "0.1.0",
+      description: "Elixir protobuf definitions for Envoy's data plane API",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers(),
       elixirc_paths: ["lib", "gen"],
       deps: deps(),
-      package: package()
+      package: package(),
+      source_url: "https://github.com/costaraphael/elixir-envoy-data-plane-api"
     ]
   end
 
@@ -25,13 +27,17 @@ defmodule EnvoyDataPlaneApi.MixProject do
       {:protobuf, "~> 0.12.0"},
       {:grpc, "~> 0.7"},
       {:google_protos, "~> 0.3"},
-      {:jason, "~> 1.0"}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README.md", "LICENSE", "gen", "Makefile"]
+      licenses: ["MIT"],
+      files: ["mix.exs", "README.md", "LICENSE", "gen", "Makefile"],
+      links: %{
+        "Github" => "https://github.com/costaraphael/elixir-envoy-data-plane-api"
+      }
     ]
   end
 end
